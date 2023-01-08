cd0da4646c6cf5789b8c05e8180ba07c806b7fcb
Ariella Giovanni Wahyuriyanto <46362798+aariella22@users.noreply.github.com>
2023-01-08 16:47:46 +0700

    Update basketball.py

--- basketball.py
+++ basketball.py
@@ -1,92 +1,89 @@
-import base64
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
 import streamlit as st
-
-st.title('# EDA BasketBall')
-
-st.sidebar.header("Input Parameters")
-selected_year = st.sidebar.selectbox("Year", list(reversed(range(1950, 2020))))
-
-"""
-parsing basketball players info from https://www.basketball-reference.com
-"""
-@st.cache
-def parse_data(year: str):
-    url = "https://www.basketball-reference.com/leagues/NBA_" + year + "_per_game.html"
-    parsed_df = pd.read_html(url, header=0)[0]
-    parsed_df = parsed_df.drop(parsed_df[parsed_df['Age'] == 'Age'].index)
-    parsed_df = parsed_df.fillna(0)
-    parsed_df = parsed_df.drop(['Rk'], axis=1)
-    # convert datatype to work with age filter
-    parsed_df['Age'] = parsed_df['Age'].astype(str).astype(int)
-    return parsed_df
+import pandas as pd
+from sklearn.datasets import make_blobs
+import plotly.express as px
+import plotly.graph_objects as go
 
 
-df_player_stat_dataset = parse_data(str(selected_year))
-sorted_dataset_by_team = sorted(df_player_stat_dataset.Tm.unique())
+def min_max_normalize(lst):
+  minimum = min(lst)
+  maximum = max(lst)
+  normalized = [(val - minimum)/(maximum - minimum) for val in lst]
+  return normalized
 
-# team filter
-selected_team = st.sidebar.multiselect("Team", sorted_dataset_by_team, sorted_dataset_by_team)
+def distance(element1 , element2):
+    x_distance = (element1[0] - element2[0])**2
+    y_distance = (element1[1] - element2[1])**2
+    return (x_distance + y_distance)**0.5
 
-# position filter
-player_positions = ['C', 'PF', 'SF', 'PG', 'SG']
-selected_position = st.sidebar.multiselect("Position", player_positions, player_positions)
-
-# multi slider
-# https://github.com/streamlit/streamlit/issues/855
-unique_age_values = df_player_stat_dataset.Age.unique()
-minValue, maxValue = min(unique_age_values), max(unique_age_values)
-
-
-# age filter
-selected_age = st.sidebar.slider("Age", int(minValue), int(maxValue), (int(minValue), int(maxValue)), 1)
-min_age, max_age = selected_age
-
-# filtered dataset
-df_selected_dataset = df_player_stat_dataset[
-    (df_player_stat_dataset.Tm.isin(selected_team) &
-     df_player_stat_dataset.Pos.isin(selected_position) &
-     df_player_stat_dataset['Age'].between(min_age, max_age))]
-
-# display dataframe
-st.header('Display Player Stats of Selected Team(s)')
-st.write(f'Data Dimension  Row : {df_selected_dataset.shape[0]} and Col : {df_selected_dataset.shape[1]}')
-st.dataframe(df_selected_dataset)
+def get_label(neighbours, y):
+    zero_count , one_count = 0,0
+    for element in neighbours:
+        if y[element[1]] == 0:
+            zero_count +=1
+        elif y[element[1]] == 1:
+            one_count +=1
+    if zero_count == one_count:
+        return y[neighbours[0][1]]
+    return 1 if one_count > zero_count else 0
 
 
-def download_dataset(dataset): # download the dataset as csv file
-    csv = dataset.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings to bytes conversions
-    href_link = f'<a href="data:file/csv;base64,{b64}" download="player_stats.csv">Download CSV File</a>'
-    return href_link
+def find_nearest(x , y , input , k):
+    distances = []
+    for id,element in enumerate(x):
+        distances.append([distance(input , element),id])
+    distances = sorted(distances)
+    predicted_label = get_label(distances[0:k] , y)
+    return predicted_label, distances[0:k] , distances[k:]
 
 
-st.markdown(download_dataset(df_selected_dataset), unsafe_allow_html=True)
+st.title("KNN Visualize")
 
-if st.button("Inter-correlation Heatmap"):
-    st.header("Inter-correlation Heatmap")
+x , y = make_blobs(n_samples = 100 , n_features = 2 , centers = 2, random_state= 2)
 
-    df_selected_dataset.to_csv('output.csv', index=False)
-    df = pd.read_csv('output.csv')
 
-    # st.dataframe(df_selected_dataset)
-    # df_selected_dataset.iloc[:, 4:].astype('float')
-    corr = df.corr()
-    # st.markdown(corr)
 
-    mask = np.zeros_like(corr)
-    mask[np.triu_indices_from(mask)] = True
+x_input = st.slider("Choose X input", min_value=0.0, max_value=1.0,key='x')
+y_input = st.slider("Choose Y input", min_value=0.0, max_value=1.0,key='y')
+k = st.slider("Choose value of K", min_value=1, max_value=10,key='k')
 
-    # plot the Inter-correlation Heatmap
+input = (x_input,y_input)
 
-    with sns.axes_style("white"):
-        f, ax = plt.subplots(figsize=(7, 5))
-        try:
-            sns.heatmap(corr, mask=mask, vmax=1, square=True)
-        except ValueError:
-            pass
-    st.pyplot(f)
+# Normalizing Data
+x[:,0] = min_max_normalize(x[:,0])
+x[:,1] = min_max_normalize(x[:,1])
+
+# Dataframe
+df = pd.DataFrame(x , columns = ['Feature1' , 'Feature2'] )
+df['Label'] = y
+
+# Initial Data Plot
+st.dataframe(df)
+fig = px.scatter(df, x = 'Feature1' , y='Feature2', symbol='Label',symbol_map={'0':'square-dot' , '1':'circle'})
+fig.add_trace(
+    go.Scatter(x= [input[0]], y=[input[1]], name = "Point to Classify", )
+)
+st.plotly_chart(fig)
+
+#Finding Nearest Neighbours
+predicted_label , nearest_neighbours, far_neighbours = find_nearest(x ,y , input ,k)
+
+st.title('Prediction')
+st.subheader('Predicted Label : {}'.format(predicted_label))
+
+nearest_neighbours = [[neighbour[1],x[neighbour[1],0],x[neighbour[1],1],neighbour[0],y[neighbour[1]]] for neighbour in nearest_neighbours]
+
+nearest_neighbours = pd.DataFrame(nearest_neighbours , columns = ['id','Feature1','Feature2','Distance','Label'])
+st.dataframe(nearest_neighbours)
+
+far_neighbours = [[neighbour[1],x[neighbour[1],0],x[neighbour[1],1],neighbour[0],y[neighbour[1]]] for neighbour in far_neighbours]
+far_neighbours = pd.DataFrame(far_neighbours , columns = ['id','Feature1','Feature2','Distance','Label'])
+
+fig2 = px.scatter(far_neighbours,x='Feature1',y='Feature2',symbol='Label',symbol_map={'0':'square-dot' , '1':'circle'})
+
+for index,neighbour in nearest_neighbours.iterrows():
+    fig2.add_trace(
+        go.Scatter( x=[input[0], neighbour['Feature1']], y=[input[1], neighbour['Feature2']],mode='lines+markers' , name = 'id {}'.format(int(neighbour['id'])) )
+    )
+
+st.plotly_chart(fig2)
